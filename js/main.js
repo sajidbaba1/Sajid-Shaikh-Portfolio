444a035ad5d8eaae4cde2cc9612b6b165cd307fc
sajid <152436427+sajidbaba1@users.noreply.github.com>
2025-06-11 17:52:30 +0530

    complete project

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// Scroll-smooth for in-page links
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', e => {
+      e.preventDefault();
+      document.querySelector(link.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
